3b3bc4ed1e546f639d606f22139df63997600319
Mateus Fernando <mateusfss368@gmail.com>
2024-11-28 12:36:51 +0000

    trocando nome de componentes para pt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { WelcomeBanner } from "@/components/Main-Sections/WelcomeBanner/welcome-banner";
-import { ServicesSection } from "@/components/Main-Sections/Catalog/catalog";
-import { ProfessionalsSection } from "@/components/Main-Sections/Professionals/professionals";
-import { TestimonialsContainer } from "@/components/Main-Sections/Testimonials/testimonials";
-import { ContactContainer } from "@/components/Main-Sections/Contact/contact";
+import { WelcomeBanner } from "@/components/Main-Sections/BemVindoBanner/welcome-banner";
+import { ServicesSection } from "@/components/Main-Sections/Catalogo/catalog";
+import { ProfessionalsSection } from "@/components/Main-Sections/Profissionais/professionals";
+import { TestimonialsContainer } from "@/components/Main-Sections/Depoimentos/testimonials";
+import { ContactContainer } from "@/components/Main-Sections/Contato/contact";
 
 export default function Home() {
   return (
