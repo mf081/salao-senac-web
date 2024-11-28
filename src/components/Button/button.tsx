f2810e2c4535bb560c1414f632d0cd2f087ba7ab
Mateus Fernando <mateusfss368@gmail.com>
2024-11-28 12:54:16 +0000

    aplicando prettier

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,6 +1,12 @@
-import * as React from 'react';
-
+import * as React from "react";
 
 export function BotaoAgendarHorario() {
-
+  <button>
+    Agendar Horário
+    <img
+      src="/img/icons/Clock-icon.png"
+      alt="Ícone de relógio"
+      className="section-01-button-icon"
+    />
+  </button>;
 }
--- src/components/Main-Sections/Catalogo/catalog.tsx
+++ src/components/Main-Sections/Catalogo/catalog.tsx
@@ -1,65 +1,82 @@
-import * as React from 'react';
+import * as React from "react";
 import "./catalog.css";
 
-export function ServicesSection() {  // Renomeando para PascalCase
-    return (
-        <section className="section-02-container">
-        <h2>Serviços</h2>
-        <h1>Ofertados</h1>
-        <div className="section-card-container">
-            <div className="card">
-                <h3>Lavada Especial</h3>
-                <p className="description">Amet minim mollit non deserunt ullamco est minim mollit.</p>
-                <p className="price">R$ 150.00</p>
-                <button id="agendar2">Agendar Horário »</button>
-            </div>
-            <div className="card">
-                <h3>Progressiva</h3>
-                <p className="description">Amet minim mollit non deserunt ullamco est minim mollit.</p>
-                <p className="price">R$ 123.00</p>
-                <button id="agendar3">Agendar Horário »</button>
-            </div>
-            <div className="card">
-                <h3>Escova</h3>
-                <p className="description">Amet minim mollit non deserunt ullamco est minim mollit.</p>
-                <p className="price">R$ 123.00</p>
-                <button id="agendar4">Agendar Horário »</button>
-            </div>
-            <div className="card">
-                <h3>Luzes</h3>
-                <p className="description">Amet minim mollit non deserunt ullamco est minim mollit.</p>
-                <p className="price">R$ 150.00</p>
-                <button id="agendar5">Agendar Horário »</button>
-            </div>
-            <div className="card">
-                <h3>Depilação</h3>
-                <p className="description">Lorem ipsum fina iadsdna.</p>
-                <p className="price">R$ 150.00</p>
-                <button id="agendar6">Agendar Horário »</button>
-            </div>
-            <div className="card">
-                <h3>Coloração</h3>
-                <p className="description">Amet minim mollit non deserunt ullamco est minim mollit.</p>
-                <p className="price">R$ 123.00</p>
-                <button id="agendar7">Agendar Horário »</button>
-            </div>
-            <div className="card">
-                <h3>Sobrancelha</h3>
-                <p className="description">Amet minim mollit non deserunt ullamco est minim mollit.</p>
-                <p className="price">R$ 123.00</p>
-                <button id="agendar8">Agendar Horário »</button>
-            </div>
-            <div className="card">
-                <h3>Selagem</h3>
-                <p className="description">Amet minim mollit non deserunt ullamco est minim mollit.</p>
-                <p className="price">R$ 123.00</p>
-                <button id="agendar9">Agendar Horário »</button>
-            </div>
+export function ServicesSection() {
+  // Renomeando para PascalCase
+  return (
+    <section className="section-02-container">
+      <h2>Serviços</h2>
+      <h1>Ofertados</h1>
+      <div className="section-card-container">
+        <div className="card">
+          <h3>Lavada Especial</h3>
+          <p className="description">
+            Amet minim mollit non deserunt ullamco est minim mollit.
+          </p>
+          <p className="price">R$ 150.00</p>
+          <button id="agendar2">Agendar Horário »</button>
         </div>
-        <div className="section-02-location">
-            <img src="/img/icons/place-icon.png" alt="Localização"/>
-            <h1>Pernambuco - Av. Visconde de Suassuna, 500 - Santo Amaro - Recife</h1>
+        <div className="card">
+          <h3>Progressiva</h3>
+          <p className="description">
+            Amet minim mollit non deserunt ullamco est minim mollit.
+          </p>
+          <p className="price">R$ 123.00</p>
+          <button id="agendar3">Agendar Horário »</button>
         </div>
+        <div className="card">
+          <h3>Escova</h3>
+          <p className="description">
+            Amet minim mollit non deserunt ullamco est minim mollit.
+          </p>
+          <p className="price">R$ 123.00</p>
+          <button id="agendar4">Agendar Horário »</button>
+        </div>
+        <div className="card">
+          <h3>Luzes</h3>
+          <p className="description">
+            Amet minim mollit non deserunt ullamco est minim mollit.
+          </p>
+          <p className="price">R$ 150.00</p>
+          <button id="agendar5">Agendar Horário »</button>
+        </div>
+        <div className="card">
+          <h3>Depilação</h3>
+          <p className="description">Lorem ipsum fina iadsdna.</p>
+          <p className="price">R$ 150.00</p>
+          <button id="agendar6">Agendar Horário »</button>
+        </div>
+        <div className="card">
+          <h3>Coloração</h3>
+          <p className="description">
+            Amet minim mollit non deserunt ullamco est minim mollit.
+          </p>
+          <p className="price">R$ 123.00</p>
+          <button id="agendar7">Agendar Horário »</button>
+        </div>
+        <div className="card">
+          <h3>Sobrancelha</h3>
+          <p className="description">
+            Amet minim mollit non deserunt ullamco est minim mollit.
+          </p>
+          <p className="price">R$ 123.00</p>
+          <button id="agendar8">Agendar Horário »</button>
+        </div>
+        <div className="card">
+          <h3>Selagem</h3>
+          <p className="description">
+            Amet minim mollit non deserunt ullamco est minim mollit.
+          </p>
+          <p className="price">R$ 123.00</p>
+          <button id="agendar9">Agendar Horário »</button>
+        </div>
+      </div>
+      <div className="section-02-location">
+        <img src="/img/icons/place-icon.png" alt="Localização" />
+        <h1>
+          Pernambuco - Av. Visconde de Suassuna, 500 - Santo Amaro - Recife
+        </h1>
+      </div>
     </section>
-    );
+  );
 }
