10a94024dcac08d4c0e3dd431c757c6e5ccb95e4
Mateus Fernando <mateusfss368@gmail.com>
2024-12-13 19:59:21 +0000

    criação de page agendamento

---
+++ src/app/pages/agendamento/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function Agendamento(){
+    return(
+        <div></div>
+    );
+}
--- src/components/Layout/Nav/nav.tsx
+++ src/components/Layout/Nav/nav.tsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import './nav.css'
+import React from 'react';
+import './nav.css';
 import Link from 'next/link';
 
-export function Nav () {
-    return(
+export function Nav() {
+    return (
         <nav>
-        <div className="nav-container">
-            <div className="nav-img">
-                <a href="/">
-                    <img src="/img/logo-salao-senac-preta.png" alt="Logo"/>
-                </a>
+            <div className="nav-container">
+                <div className="nav-img">
+                    <Link href="/">
+                        <img src="/img/logo-salao-senac-preta.png" alt="Logo" />
+                    </Link>
+                </div>
+                <div className="nav-right">
+                    <Link href="/">
+                        <button className="nav-button">HOME</button>
+                    </Link>
+                    <Link href="pages/agendamento">
+                        <button className="nav-button">AGENDAR HORÁRIO</button>
+                    </Link>
+                    <a href="#AdressSection">
+                        <button className="nav-button">CONTATO</button>
+                    </a>
+                    <a href="#AboutUsSection">
+                        <button className="nav-button">SOBRE NÓS</button>
+                    </a>
+                </div>
             </div>
-            <div className="nav-right">
-                <a href="/" ><button className="nav-button">HOME</button></a>
-                <a href="#contato" ><button className="nav-button">AGENDAR HORÁRIO</button></a>
-                <a href="#AdressSection" ><button className="nav-button">CONTATO</button></a>
-                <a href="#AboutUsSection" ><button className="nav-button">SOBRE NÓS</button></a>
-            </div>
-        </div>
-    </nav>  
+        </nav>
     );
 }
