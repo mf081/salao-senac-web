4547afeefd525ae21387c9aa40f0b9f1707d71e5
Victor Oliveira <166130816+victolvr@users.noreply.github.com>
2024-12-14 12:34:39 +0000

    ajustes na tela de cadastramento

--- src/app/pages/LoginCliente/Cadastramento/page.tsx
+++ src/app/pages/LoginCliente/Cadastramento/page.tsx
@@ -12,52 +12,84 @@ export default function Cadastramento() {
                     <h2>Cadastre-se</h2>
                     <form action="/login" method="POST">
                         <div className="forms">
-                            <label htmlFor="nome">Nome Completo ou Social</label>
-                            <input 
-                                type="text" 
-                                id="nome" 
-                                name="nome" 
-                                placeholder="Informe seu nome completo ou social" 
-                                required 
-                            />
+                            {/* Primeira linha: Nome e Email */}
+                            <div className="forms-colunas">
+                                <div className="input-group">
+                                    <label htmlFor="nome">Nome Completo*</label>
+                                    <input 
+                                        type="text" 
+                                        id="nome" 
+                                        name="nome" 
+                                        placeholder="Informe seu nome completo" 
+                                        required 
+                                    />
+                                </div>
+                                <div className="input-group">
+                                    <label htmlFor="nome">Nome Social</label>
+                                    <input 
+                                        type="text" 
+                                        id="nome" 
+                                        name="nome" 
+                                        placeholder="Informe seu nome social" 
+                                        required 
+                                    />
+                                    
+                                </div>
+                            </div>
 
-                            <label htmlFor="email">Email</label>
-                            <input 
-                                type="email" 
-                                id="email" 
-                                name="email" 
-                                placeholder="Informe seu email" 
-                                required 
-                            />
+                            {/* Segunda linha: Senha e CPF */}
+                            <div className="forms-colunas">
+                                <div className="input-group">
+                                    <label htmlFor="email">Email*</label>
+                                    <input 
+                                        type="email" 
+                                        id="email" 
+                                        name="email" 
+                                        placeholder="Informe seu email" 
+                                        required 
+                                    />  
+                                    
+                                </div>
+                                <div className="input-group">
+                                    <label htmlFor="password">Senha*</label>
+                                    <input 
+                                        type="password" 
+                                        id="password" 
+                                        name="password" 
+                                        placeholder="Escreva sua senha" 
+                                        required 
+                                    />
+                                    
+                                </div>
+                            </div>
 
-                            <label htmlFor="password">Senha</label>
-                            <input 
-                                type="password" 
-                                id="password" 
-                                name="password" 
-                                placeholder="Escreva sua senha" 
-                                required 
-                            />
+                            {/* Terceira linha: Telefone */}
+                            <div className="forms-colunas">
+                            <div className="input-group">
+                                <label htmlFor="telefone">Telefone*</label>
+                                <input 
+                                    type="tel" 
+                                    id="telefone" 
+                                    name="telefone" 
+                                    placeholder="Informe seu telefone" 
+                                    required 
+                                />
+                               
+                            </div>
+                            <div className="input-group">
+                                <label htmlFor="cpf">CPF*</label>
+                                <input 
+                                        type="text" 
+                                        id="cpf" 
+                                        name="cpf" 
+                                        placeholder="Informe seu CPF" 
+                                        required 
+                                />
+                            </div>
+                        </div>
 
-                            <label htmlFor="cpf">CPF</label>
-                            <input 
-                                type="text" 
-                                id="cpf" 
-                                name="cpf" 
-                                placeholder="Informe seu CPF" 
-                                required 
-                            />
+                            <button type="submit" className="button-entrar-login">Cadastrar</button>
 
-                            <label htmlFor="telefone">Telefone</label>
-                            <input 
-                                type="tel" 
-                                id="telefone" 
-                                name="telefone" 
-                                placeholder="Informe seu telefone" 
-                                required 
-                            />
-
-                            <button type="submit" className="button-entrar-login">Entrar</button>
                             <div className='paragrafo-login'>
                                 <p>
                                     Já tem Cadastro? <a href="/pages/LoginCliente/Cadastramento" className='cadastre-login'>Clique Aqui</a>
