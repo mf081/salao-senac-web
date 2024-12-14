21a2e6cc7e454615be64e7ea57f62f37e6622583
Mateus Fernando <mateusfss368@gmail.com>
2024-12-14 08:47:30 +0000

    calendario versão 1

--- src/app/pages/AgendamentoCliente/Agendamento/page.tsx
+++ src/app/pages/AgendamentoCliente/Agendamento/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Agendamento.css'
 import SideNavBar from '@/components/UI/SideNavBar/SideNavBar';
+import { Calendar } from '@/components/UI/Calendar/Calendar';
 
 
 export default function Agendamento() {
     return (
       <main className='agendamento-main'>
         <SideNavBar></SideNavBar>
+        <Calendar></Calendar>
       </main>
     );
   }
---
+++ src/components/UI/Calendar/Calendar.tsx
@@ -0,0 +1,75 @@
+'use client';
+import React, { useState, useCallback } from "react";
+import "./Calendar.css";
+import calendarData from '/workspaces/salao-senac-web/src/json/calendar.json';
+
+type Dia = {
+  data: string;
+};
+
+type Mes = {
+  nome: string;
+  ano: number;
+  dias: Dia[];
+};
+
+export function Calendar() {
+  const [currentMonthIndex, setCurrentMonthIndex] = useState(0);
+
+  const goToMonth = useCallback((direction: number) => {
+    setCurrentMonthIndex((prevIndex) => {
+      const nextIndex = (prevIndex + direction + calendarData.meses.length) % calendarData.meses.length;
+      return nextIndex;
+    });
+  }, []);
+
+  const currentMonth: Mes = calendarData.meses[currentMonthIndex];
+
+  const weekdays = ["S", "D", "S", "T", "Q", "Q", "S"]; // Siglas dos dias da semana
+
+  const MonthNavigation = () => (
+    <div id="CalendarSection" className="text-card-categories-container">
+      <h3 className="text-card-categories">{`${currentMonth.nome}`}</h3>
+      <div className="arrows-img-keys">
+        <img
+          className="arrow-img"
+          src="/img/icons/icon-seta-esquerda.png"
+          width="100px"
+          height="100px"
+          onClick={() => goToMonth(-1)}
+          alt="Seta para a esquerda"
+        />
+        <img
+          className="arrow-img"
+          src="/img/icons/icon-seta-direita.png"
+          width="100px"
+          height="100px"
+          onClick={() => goToMonth(1)}
+          alt="Seta para a direita"
+        />
+      </div>
+    </div>
+  );
+
+  return (
+    <section className="section-02-container">
+      <MonthNavigation />
+
+      {/* Siglas dos dias da semana */}
+      <div className="weekdays">
+        {weekdays.map((day, index) => (
+          <div className="weekday" key={index}>{day}</div>
+        ))}
+      </div>
+
+      {/* Exibindo os dias do mês */}
+      <div className="section-card-container">
+        {currentMonth.dias.map((dia, index) => (
+          <div className="card" key={`${currentMonthIndex}-${index}`}>
+            <h3>{new Date(dia.data).getDate()}</h3> {/* Apenas o número do dia */}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
