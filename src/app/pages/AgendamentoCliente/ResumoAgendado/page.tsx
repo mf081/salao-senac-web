7c11bd9ee1dd5b2861726b4441ff41959e0c71b0
dilucas00 <165342425+dilucas00@users.noreply.github.com>
2024-12-15 03:05:09 +0000

    finalizada a seção resumo agendado

--- src/app/pages/AgendamentoCliente/ResumoAgendado/page.tsx
+++ src/app/pages/AgendamentoCliente/ResumoAgendado/page.tsx
@@ -14,9 +14,13 @@ export default function ResumoAgendado (){
                         <h2>Resumo da compra</h2>
                          <div className="info">
                             <p><strong>Serviço:</strong> Progressiva + Escova R$ 246,00</p>
+                            <br />
                             <p><strong>Data:</strong> 18/11/2024</p>
+                            <br />
                             <p><strong>Horário:</strong> 10:30 - 11:15 <br></br> 11:15 - 12:00</p>
-                            <p><strong>Forma de Pag.:</strong> Pix</p>
+                            <br />
+                            <p><strong>Forma de Pag:</strong> Pix</p>
+                            <br />
                             <hr></hr>
                             <p className="total"><strong>Total</strong> R$ 90,00</p>
                         </div>
