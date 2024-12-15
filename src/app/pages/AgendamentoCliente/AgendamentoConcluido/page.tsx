3492e2570767a5686d00adf33abcab195d485c35
dilucas00 <165342425+dilucas00@users.noreply.github.com>
2024-12-15 13:17:37 +0000

    atualização na tela agendamento concluido

--- src/app/pages/AgendamentoCliente/AgendamentoConcluido/page.tsx
+++ src/app/pages/AgendamentoCliente/AgendamentoConcluido/page.tsx
@@ -1,37 +1,43 @@
 import React from 'react';
 import './AgendamentoConcluido.css'
+import SideNavBar from '@/components/Layout/SideNavBar/SideNavBar';
 
 export default function AgendamentoConcluido (){
     return(
         <main>
-                    <section className="summary-box">
-                        <h2>Agendamento realizado com sucesso !</h2>
-                        <p>O pagamento deve ser realizado no local</p>
+            <SideNavBar></SideNavBar>
+            <section className="summary-box">
+                <div className='.container-sumary'>
 
-                        <div className="info-cards">
-                            <div className="card">
-                                <p className="label">Id agendamento:</p>
-                                <p className="value">000511004</p>
-                        </div>
+                    <h2>Agendamento realizado com sucesso !</h2>
+                    <p>O pagamento deve ser realizado no local</p>
+
+                    <div className="info-cards">
                         <div className="card">
-                            <p className="label">Data agendamento:</p>
-                            <p className="value">18/11/2024</p>
+                            <p className="label">Id agendamento:</p>
+                            <p className="value">000511004</p>
                         </div>
-                        <div className="card">
-                            <p className="label">Serviço:</p>
-                            <p className="value">Progressiva + Escova</p>
-                        </div>
-                        <div className="card">
-                            <p className="label">Funcionário(a):</p>
-                            <p className="value">Marta Menezes</p>
-                        </div>
+                    <div className="card">
+                        <p className="label">Data agendamento:</p>
+                        <p className="value">18/11/2024</p>
+                    </div>
+                    <div className="card">
+                        <p className="label">Serviço:</p>
+                        <p className="value">Progressiva + Escova</p>
+                    </div>
+                    <div className="card">
+                        <p className="label">Funcionário(a):</p>
+                        <p className="value">Marta Menezes</p>
+                    </div>
                     </div>
 
                     <div className="download">
                         <p>Gere seu PDF</p>
                         <span className="icon">⬇</span>
                     </div>
-                </section>
+            
+                </div>
+            </section>
         </main>
     );
 }
