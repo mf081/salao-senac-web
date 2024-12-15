a776576f1a3e34ecf43379091f735dbbd54afb3d
dilucas00 <165342425+dilucas00@users.noreply.github.com>
2024-12-15 21:40:53 +0000

    alterações na selçao de agendamento concluido

--- src/app/pages/AgendamentoCliente/AgendamentoConcluido/page.tsx
+++ src/app/pages/AgendamentoCliente/AgendamentoConcluido/page.tsx
@@ -1,6 +1,9 @@
+
 import React from 'react';
 import './AgendamentoConcluido.css'
 import SideNavBar from '@/components/Layout/SideNavBar/SideNavBar';
+import DownloadButton from '/workspaces/salao-senac-web/src/components/Button/ButtonDownloadPDF/DownloadButton'; // Corrigi o caminho para a importação
+
 
 export default function AgendamentoConcluido (){
     return(
@@ -14,26 +17,29 @@ export default function AgendamentoConcluido (){
 
                     <div className="info-cards">
                         <div className="card">
-                            <p className="label">Id agendamento:</p>
-                            <p className="value">000511004</p>
+                                <p className="label">Id agendamento:</p>
+                                <p className="value">000511004</p>
                         </div>
-                    <div className="card">
-                        <p className="label">Data agendamento:</p>
-                        <p className="value">18/11/2024</p>
-                    </div>
-                    <div className="card">
-                        <p className="label">Serviço:</p>
-                        <p className="value">Progressiva + Escova</p>
-                    </div>
-                    <div className="card">
-                        <p className="label">Funcionário(a):</p>
-                        <p className="value">Marta Menezes</p>
-                    </div>
+                        <div className="card">
+                            <p className="label">Data agendamento:</p>
+                            <p className="value">18/11/2024</p>
+                        </div>
+                        <div className="card">
+                            <p className="label">Serviço:</p>
+                            <p className="value">Progressiva + Escova</p>
+                        </div>
+                        <div className="card">
+                            <p className="label">Funcionário(a):</p>
+                            <p className="value">Marta Menezes</p>
+                        </div>
                     </div>
 
                     <div className="download">
                         <p>Gere seu PDF</p>
-                        <span className="icon">⬇</span>
+                        <DownloadButton>
+                            
+                        </DownloadButton>
+                            
                     </div>
             
                 </div>
---
+++ src/components/Button/ButtonDownloadPDF/DownloadButton.tsx
@@ -0,0 +1,21 @@
+'use client';
+import React from "react";
+export default function DownloadButton () {
+    return(
+        <a
+        href="/comprovante-agendamento.pdf"
+        download='comprovantegendamento.pdf'
+        
+        >
+            
+        <span>⬇</span>
+        </a>
+        
+            
+        
+    )
+       
+
+
+}
+
