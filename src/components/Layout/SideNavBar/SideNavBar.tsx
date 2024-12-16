f6957e9d6b491e7a1b4c70576a51c54d52a31557
Mateus Fernando <mateusfss368@gmail.com>
2024-12-16 09:02:55 +0000

    bora

--- src/components/Layout/SideNavBar/SideNavBar.tsx
+++ src/components/Layout/SideNavBar/SideNavBar.tsx
@@ -1,15 +1,18 @@
 'use client';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './SideNavBar.css';
 import { useRouter } from 'next/navigation';
 import AvatarProfile from '@/components/UI/AvatarProfile/AvatarProfile';
 
 export default function SideNavBar() {
+  const [nomeUsuario, setNomeUsuario] = useState('Usuário');
   const router = useRouter();
 
-  // Obtém o nome do usuário do localStorage
-  const storedUser = JSON.parse(localStorage.getItem('user') || '{}');
-  const nomeUsuario = storedUser.nome || 'Usuário';
+  // Carregar o nome do usuário do localStorage após a montagem do componente
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem('user') || '{}');
+    setNomeUsuario(storedUser.nome || 'Usuário');
+  }, []); // Executa apenas uma vez, após a montagem
 
   const handleEditarPerfilClick = () => {
     router.push('/pages/AgendamentoCliente/EditarPerfil');
@@ -18,21 +21,27 @@ export default function SideNavBar() {
   const handleHistoricoClick = () => {
     router.push('/pages/AgendamentoCliente/HistoricoAgendamento');
   };
+
   const handleHomeClick = () => {
     router.push('/'); // Voltar para a página inicial
-};
+  };
 
   return (
     <main>
       <nav className="sidebar">
-
-        <AvatarProfile></AvatarProfile>
+        <AvatarProfile />
         <span className="sidebar-username">Seja Bem Vindo {nomeUsuario}!</span>
 
         <div className="buttons">
-          <button onClick={handleEditarPerfilClick} className="button-side-bar">Editar Perfil</button>
-          <button onClick={handleHistoricoClick} className="button-side-bar">Histórico</button>
-          <button onClick={handleHomeClick} className="button-side-bar">Sair</button>
+          <button onClick={handleEditarPerfilClick} className="button-side-bar">
+            Editar Perfil
+          </button>
+          <button onClick={handleHistoricoClick} className="button-side-bar">
+            Histórico
+          </button>
+          <button onClick={handleHomeClick} className="button-side-bar">
+            Sair
+          </button>
         </div>
       </nav>
     </main>
